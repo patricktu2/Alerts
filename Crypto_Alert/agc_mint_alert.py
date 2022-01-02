5212e184ce06d97947ea77e722eb41eab1a00dc3
Patrick <patricktu93@gmail.com>
2022-01-03 00:07:54 +0100

    add tracker for snipe targets

--- Crypto_Alert/agc_mint_alert.py
+++ Crypto_Alert/agc_mint_alert.py
@@ -3,13 +3,21 @@ import time
 from time import sleep
 from Functions.file_handler import save_pickle, load_pickle
 from Functions.telegrambot import telegram_bot_sendtext, etherscan_api_key, bot_chatID_private
+from dotenv import load_dotenv
+import os 
+import numpy as np
 
 NAME        = 'Alpha Girl Club'
 PICKLE_FILE = '../Data/agc_last_counter.pickle'
 ADDRESS     = '0x8c5029957bf42c61d19a29075dc4e00b626e5022'   # Alpha Girl Club
 OPENSEA     = 'alphagirlclub'
-SLEEP       = 3600
+SLEEP       = 300
+BOT_CHAT_ID_AGC = str(os.getenv('TELEGRAM_BOT_CHATID_AGC'))    # Replace with your own bot_chatID
 
+
+SNIPE_TARGETS = [ # unique alpha girls
+    7939, 8368, 8436, 8674, 9328, 9481, 9496
+]
 
 def get_last_message():
     dict_last_messages = load_pickle(PICKLE_FILE)
@@ -80,12 +88,12 @@ def getETHprice():
     market = format(market, ",")
 
     priceinfo = f"""<b><ins><a href='https://coingecko.com/en/coins/ethereum/'>Ethereum | $ETH</a> Price:</ins></b>
-<b>💰 EUR:</b> €{peur}
-<b>💰 USD:</b> ${pusd}
-<b>🗿 BTC:</b> ฿{pbtc}
-<b>📈 24h change:</b> {pchange}%
-<b>💎 Market Cap:</b> ${market}
-"""
+                <b>💰 EUR:</b> €{peur}
+                <b>💰 USD:</b> ${pusd}
+                <b>🗿 BTC:</b> ฿{pbtc}
+                <b>📈 24h change:</b> {pchange}%
+                <b>💎 Market Cap:</b> ${market}
+                """
 
     return peur_val, pusd_val, priceinfo
 
@@ -97,32 +105,43 @@ def getOSstats(collection=OPENSEA):
 
     return stats
 
+def get_next_snipe_target(current_counter, snipe_target_list):
+    sorted_target_list = np.sort(snipe_target_list)
+    for snipe_target in sorted_target_list:
+        if snipe_target > current_counter:
+            return snipe_target
+
+    return None
 
 def run_mint_counter():
     stats = getOSstats()
     dict_data    = getEtherScanData()
     last_counter = get_last_message()
-    mint_counter = int(stats['count'])
+    mint_counter = int(stats['count']) -1 # as there is a test nft #0
     console_output  = NAME + ': Last ' + str(last_counter) + ' | Now ' + str(mint_counter)
     print(console_output)
+
+    next_snipe_target = get_next_snipe_target(mint_counter, SNIPE_TARGETS)
+
     if mint_counter - last_counter > 0:
         maxSupply = 9500
         amount_left = maxSupply - mint_counter
+        amount_left_to_target = next_snipe_target - mint_counter if next_snipe_target else "No next snipe target"
         stats = getOSstats()
         owner_mint_ratio = round(float(mint_counter/stats['num_owners']), 2)
-        message  = 'Minted: *' + str(mint_counter) + '* | Holders: *' + str(stats['num_owners']) + '*'
-        message += '\nOwner-to-Mint-Ratio: *' + str(owner_mint_ratio) + '*'
-        message += '\nLeft: *' + str(amount_left) + '*'
-        message += '\n\nFloor Price: *' + str(stats['floor_price']) + ' ETH*'
+        
+        message  = 'Minted: *' + str(mint_counter) + '* | Holders: *' + str(stats['num_owners']) + '* | Left: *' + str(amount_left) + '*'
+        message += '\nNext Snipe Target: *' + str(next_snipe_target) + '* | Left to target: *' + str(amount_left_to_target) + '*'
+        message += '\nFloor Price: *' + str(stats['floor_price']) + ' ETH*'
         message += '\nVolume traded: *' + str(int(stats['total_volume'])) + ' ETH*'
         price = getCurrentMintPrice(dict_data)
         eur, usd, _ = getETHprice()
         eur_price = int(eur * price)
         usd_price = int(usd * price)
         message += '\n\nCurrent Mint Price: *' + str(price) + ' ETH* (' + str(eur_price) + ' EUR | ' + str(usd_price) + ' USD)'
-        message    += '\n\n-----\nIf you have any issues or feedback, feel free to [contact me](tg://user?id=383615621) :)'
-        message  += '\nCheck out my other [Telegram-Bots](https://linktr.ee/v1et4nh)'
-        telegram_bot_sendtext(message, bot_chatID='-1001655992736', disable_web_page_preview=True)
+        message  += '\n Mint at (https://mint.alphagirlclub.io/)'
+
+        telegram_bot_sendtext(message, bot_chatID=BOT_CHAT_ID_AGC, disable_web_page_preview=True)
         dict_counter = {'counter': mint_counter}
         save_pickle(dict_counter, PICKLE_FILE)
 
